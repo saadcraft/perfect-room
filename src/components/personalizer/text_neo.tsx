831042e3dacd5ac4a01369caff123fef63035b46
saadcraft <39559368+saadcraft@users.noreply.github.com>
2025-02-11 22:52:10 +0100

    style personalizé

--- src/components/personalizer/text_neo.tsx
+++ src/components/personalizer/text_neo.tsx
@@ -7,31 +7,33 @@ export default function NeonSign() {
     const [textColor, setTextColor] = useState<string[]>(["#0800ff", "#cbc9ff"]);
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen p-4">
-            {/* Transparent SVG Neon Text with Separate Glow Color */}
-            <svg width="100%" height="200">
-                <defs>
-                    <filter id="neon-glow">
-                        {/* Create the glow effect using a separate glow color */}
-                        <feDropShadow dx="0" dy="0" stdDeviation={5} floodColor={textColor[0]} />
-                    </filter>
-                </defs>
-                <text
-                    x="50%"
-                    y="50%"
-                    fontSize="80"
-                    textAnchor="middle"
-                    dominantBaseline="middle"
-                    fill={textColor[1]}
-                    filter="url(#neon-glow)"
-                    className="neon-text"
-                >
-                    {text}
-                </text>
-            </svg>
+        <div className="relative top-20 md:top-0 flex md:flex-row flex-col items-center gap-4 md:justify-between min-h-screen p-4">
+            <div className="md:w-2/3">
+                {/* Transparent SVG Neon Text with Separate Glow Color */}
+                <svg width="100%" className="relative md:-top-20 max-w-7xl mx-auto">
+                    <defs>
+                        <filter id="neon-glow">
+                            {/* Create the glow effect using a separate glow color */}
+                            <feDropShadow dx="0" dy="0" stdDeviation={5} floodColor={textColor[0]} />
+                        </filter>
+                    </defs>
+                    <text
+                        x="50%"
+                        y="50%"
+                        fontSize="50"
+                        textAnchor="middle"
+                        dominantBaseline="middle"
+                        fill={textColor[1]}
+                        filter="url(#neon-glow)"
+                        className="neon-text"
+                    >
+                        {text}
+                    </text>
+                </svg>
+            </div>
 
             {/* Controls */}
-            <form className="flex flex-col items-center gap-4 mt-6">
+            <form className="flex flex-col items-center gap-10 mt-6">
                 {/* Text Input */}
                 <input
                     type="text"
@@ -62,21 +64,41 @@ export default function NeonSign() {
                         className="w-10 h-10 border-none"
                     />
                 </label> */}
-                <div className="flex gap-3">
-                    <div className="w-full">
+                <div className="grid grid-cols-3 gap-y-6">
+                    <div>
                         <input type="radio" value="blue" id="blue" defaultChecked name="color" onChange={() => setTextColor(["#0800ff", "#cbc9ff"])} className="peer hidden" />
-                        <label htmlFor="blue" className='cursor-pointer border rounded-lg text-slate-400 peer-checked:text-[#0800ff] text-nowrap peer-checked:border-[#0800ff] p-2'> Blue</label>
+                        <label htmlFor="blue" className='w-24 cursor-pointer border rounded-lg text-slate-400 peer-checked:text-[#0800ff] peer-checked:border-[#0800ff] p-2'> Blue</label>
                     </div>
 
                     <div>
                         <input type="radio" value="green" id="green" name="color" onChange={() => setTextColor(["#00ff44", "#dbffe5"])} className="peer hidden" />
-                        <label htmlFor="green" className='h-2 w-11 cursor-pointer border rounded-lg text-slate-400 peer-checked:text-[#00ff44] text-nowrap peer-checked:border-[#00ff44] p-2'> Vert</label>
+                        <label htmlFor="green" className='h-2 w-11 cursor-pointer border rounded-lg text-slate-400 peer-checked:text-[#00ff44] peer-checked:border-[#00ff44] p-2'> Vert</label>
                     </div>
                     <div>
                         <input type="radio" value="red" id="red" name="color" onChange={() => setTextColor(["#ff000d", "#fcc2c5"])} className="peer hidden" />
                         <label htmlFor="red" className='cursor-pointer border rounded-lg text-slate-400 peer-checked:text-[#ff000d] text-nowrap peer-checked:border-[#ff000d] p-2'> Rouge</label>
                     </div>
+                    <div>
+                        <input type="radio" value="white" id="white" name="color" onChange={() => setTextColor(["#fff", "#fff"])} className="peer hidden" />
+                        <label htmlFor="white" className='cursor-pointer border rounded-lg text-slate-400 peer-checked:text-[#fff] text-nowrap peer-checked:border-[#fff] p-2'> blanc</label>
+                    </div>
+                    <div>
+                        <input type="radio" value="yellow" id="yellow" name="color" onChange={() => setTextColor(["#fcdb03", "#fff3a3"])} className="peer hidden" />
+                        <label htmlFor="yellow" className='cursor-pointer border rounded-lg text-slate-400 peer-checked:text-[#fcdb03] text-nowrap peer-checked:border-[#fcdb03] p-2'> Jaune</label>
+                    </div>
+                    <div>
+                        <input type="radio" value="orange" id="orange" name="color" onChange={() => setTextColor(["#ff9100", "#ffd296"])} className="peer hidden" />
+                        <label htmlFor="orange" className='cursor-pointer border rounded-lg text-slate-400 peer-checked:text-[#ff9100] text-nowrap peer-checked:border-[#ff9100] p-2'> Orange</label>
+                    </div>
+                    <div>
+                        <input type="radio" value="pink" id="pink" name="color" onChange={() => setTextColor(["#f200fa", "#fee6ff"])} className="peer hidden" />
+                        <label htmlFor="pink" className='cursor-pointer border rounded-lg text-slate-400 peer-checked:text-[#f200fa] text-nowrap peer-checked:border-[#f200fa] p-2'> Rose</label>
+                    </div>
                 </div>
+
+                <select className="text-black px-3 py-2 rounded-md border border-gray-300">
+                    <option>Sélectioné font</option>
+                </select>
 
                 {/* Glow Intensity Slider */}
 
