1215410e81a1159868765e68c6f07993fabfbf61
saadcraft <39559368+saadcraft@users.noreply.github.com>
2025-02-19 00:03:45 +0100

    update designe

--- src/components/personalizer/text_neo.tsx
+++ src/components/personalizer/text_neo.tsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useRef, useState } from "react";
 
+// import Image from "next/image";
 import { Lexend, Yellowtail, Tourney, Monoton } from "next/font/google";
 import localFont from "next/font/local";
 
@@ -36,27 +37,30 @@ const meowScript = localFont({
     display: "swap",
 })
 
-const colors = [
-    { id: "blue", colors: ["#0800ff", "#cbc9ff"], label: "Blue" },
-    { id: "green", colors: ["#00ff44", "#dbffe5"], label: "Vert" },
-    { id: "red", colors: ["#ff000d", "#fcc2c5"], label: "Rouge" },
-    { id: "white", colors: ["#fff", "#fff"], label: "Blanc" },
-    { id: "yellow", colors: ["#fcdb03", "#fff3a3"], label: "Jaune" },
-    { id: "orange", colors: ["#ff9100", "#ffd296"], label: "Orange" },
-    { id: "pink", colors: ["#f200fa", "#fee6ff"], label: "Rose" },
-]
+// const colors = [
+//     { id: "blue", colors: ["#0800ff", "#cbc9ff"], label: "Blue" },
+//     { id: "green", colors: ["#00ff44", "#dbffe5"], label: "Vert" },
+//     { id: "red", colors: ["#ff000d", "#fcc2c5"], label: "Rouge" },
+//     { id: "white", colors: ["#fff", "#fff"], label: "Blanc" },
+//     { id: "yellow", colors: ["#fcdb03", "#fff3a3"], label: "Jaune" },
+//     { id: "orange", colors: ["#ff9100", "#ffd296"], label: "Orange" },
+//     { id: "pink", colors: ["#f200fa", "#fee6ff"], label: "Rose" },
+// ]
 
 export default function NeonSign() {
     const [text, setText] = useState("Custom Neon");
     const [textColor, setTextColor] = useState<string[]>(["#0800ff", "#cbc9ff"]);
     const [font, setFont] = useState<string>("Courier New");
-    const [range, setRange] = useState<number>(10);
-    const [svgHeight, setSvgHeight] = useState<number | string>("auto"); // State to store SVG height
-    const [svgWidth, setSvgWidth] = useState<number | string>("auto"); // State to store SVG height
+    const [range, setRange] = useState<number>(5);
+    const [svgHeight, setSvgHeight] = useState<number | null>(null); // State to store SVG height
+    const [svgWidth, setSvgWidth] = useState<number | null>(null); // State to store SVG height
     const [count, setCount] = useState<number[]>([0, 0])
     const [fontSize, setFontSize] = useState<number>(50); // State for dynamic font size
 
     const svgRef = useRef<SVGSVGElement>(null); // Ref to the SVG element
+
+    const l = ((count[1] * 8) / 96 * 2.54 * range);
+    const h = ((count[0] * 5) / 96 * 2.54 * range);
 
     useEffect(() => {
         const handleResize = () => {
@@ -96,41 +100,61 @@ export default function NeonSign() {
         Lexend: lexend.className,
     };
 
+    const calculateArea = () => {
+        const lengthInMeters = (l || 0) / 100;
+        const heightInMeters = (h || 0) / 100;
+        console.log("mafhamt walo", lengthInMeters)
+        console.log("mafhamt walo2", heightInMeters)
+        return ((lengthInMeters * heightInMeters) * 6700).toFixed(2);
+    };
+
     return (
-        <div className="relative md:top-0 flex md:flex-row flex-col mb-2 items-center gap-4 md:justify-between min-h-screen md:p-4">
-            <div className="fixed md:relative z-40 md:w-2/3 w-full h-1/3 md:h-auto md:mt-20 mt-16 bg-black py-3">
-                {/* Transparent SVG Neon Text with Separate Glow Color */}
-                <div className="md:-mt-44 mt-10 w-full overflow-auto">
-                    <svg ref={svgRef} width={Number(svgWidth) + 20 || "auto"} height={Number(svgHeight) + 20 || "auto"} className="max-w-7xl mx-auto">
-                        <defs>
-                            <filter id="neon-glow">
-                                {/* Create the glow effect using a separate glow color */}
-                                <feDropShadow dx="0" dy="0" stdDeviation={10} floodColor={textColor[0]} />
-                            </filter>
-                        </defs>
-                        <text
-                            x="50%"
-                            y="50%"
-                            fontSize={fontSize}
-                            textAnchor="middle"
-                            dominantBaseline="middle"
-                            fill={textColor[1]}
-                            filter="url(#neon-glow)"
-                            className="neon-text"
-                            style={{ fontFamily: font }}
-                        >
-                            {text}
-                        </text>
-                    </svg>
-                </div>
-                <p className='absolute bottom-3 right-3 text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 to-pink-500 font-bold text-3xl'>1000 DA</p>
-                <div className="absolute left-3 bottom-3 flex gap-3 text-white md:text-lg text-sm">
-                    <p className="text-center mt-4">L: {((count[1] * 9) / 96 * 2.54 * range).toFixed(2)}cm</p>
-                    <p className="text-center mt-4">H: {((count[0] * 6) / 96 * 2.54 * range).toFixed(2)}cm</p>
+        <div className="relative md:top-0 flex md:flex-row flex-col mb-2 items-start gap-4 md:justify-between min-h-screen md:p-4">
+            <div className="relative w-full h-72 md:h-screen top-0">
+                <div
+                    className="fixed md:absolute z-40 top-20 bottom-0 right-0 left-0 rounded-b-2xl md:rounded-2xl w-full h-1/3 md:h-5/6 py-3"
+                    style={{
+                        backgroundImage: 'url(/images/wall.jpg)', // Set the background image
+                        backgroundSize: 'cover', // Ensure the image covers the entire div
+                        backgroundPosition: 'bottom', // Center the image
+                        backgroundRepeat: 'no-repeat', // Prevent the image from repeating
+                    }}
+                >
+                    {/* Transparent SVG Neon Text with Separate Glow Color */}
+                    <div className="md:mt-20 mt-10 w-full overflow-auto">
+                        <svg ref={svgRef} width={svgWidth ? svgWidth + 20 : "auto"} height={svgHeight ? svgHeight + 20 : "auto"} className="max-w-7xl mx-auto">
+                            <defs>
+                                <filter id="neon-glow">
+                                    {/* Create the glow effect using a separate glow color */}
+                                    <feDropShadow dx="0" dy="0" stdDeviation={5} floodColor={textColor[0]} />
+                                </filter>
+                            </defs>
+                            <text
+                                x="50%"
+                                y="50%"
+                                fontSize={fontSize}
+                                textAnchor="middle"
+                                dominantBaseline="middle"
+                                fill={textColor[1]}
+                                filter="url(#neon-glow)"
+                                className="neon-text"
+                                style={{ fontFamily: font }}
+                            >
+                                {text}
+                            </text>
+                        </svg>
+                    </div>
+                    <div className="absolute w-full md:bottom-40 bottom-0 p-4 flex items-end justify-between">
+                        <div className="flex gap-3 pb-1.5 text-white md:text-lg text-sm">
+                            <p className="text-center mt-4">L: {l.toFixed(2)}cm</p>
+                            <p className="text-center mt-4">H: {h.toFixed(2)}cm</p>
+                        </div>
+                        <p className='text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 to-pink-500 font-bold text-lg md:text-3xl'>{calculateArea()} DA</p>
+                    </div>
                 </div>
             </div>
             {/* Controls */}
-            <form className="relative top-2 mt-72 flex flex-col rounded-2xl text-white items-center w-full md:w-auto gap-7 md:mt-6 bg-slate-800 px-4 py-2">
+            <form className="relative md:top-10 pb-6 top-4 mb-2 flex flex-col rounded-2xl text-white items-center w-full md:w-auto gap-4 md:mt-6 bg-slate-800 px-4 py-2">
                 {/* Text Input */}
 
                 <p>Entre le text</p>
@@ -139,12 +163,12 @@ export default function NeonSign() {
                     value={text}
                     onChange={(e) => setText((e.target.value))}
                     placeholder="Enter text"
-                    className="text-black px-3 py-2 rounded-md border border-gray-300"
+                    className="text-black px-3 py-2 w-full rounded-md border border-gray-300"
                 />
 
                 {/* select color */}
-                <p>sélection couleurs</p>
-                <div className="grid grid-cols-3 gap-3">
+                <p>sélectionné couleurs</p>
+                <div className="grid md:grid-cols-3 grid-cols-4 gap-3">
                     <div>
                         <input type="radio" value="blue" id="blue" defaultChecked name="color" onChange={() => setTextColor(["#0800ff", "#cbc9ff"])} className="peer hidden" />
                         <label htmlFor="blue" className='w-14 flex items-center justify-center cursor-pointer border rounded-lg text-slate-400 peer-checked:text-[#0800ff] peer-checked:border-[#0800ff] p-2'> Blue</label>
@@ -176,7 +200,19 @@ export default function NeonSign() {
                     </div>
                 </div>
 
-                <p>sélection font</p>
+                <p>sélectionné matière</p>
+                <div className="flex gap-2">
+                    <div>
+                        <input type="radio" value="pmma" id="pmma" name="materiel" className="peer hidden" />
+                        <label htmlFor="pmma" className='w-14 flex items-center justify-center cursor-pointer border rounded-lg text-slate-400 peer-checked:text-indigo-500 text-nowrap peer-checked:border-indigo-500 p-2'> PMMA</label>
+                    </div>
+                    <div>
+                        <input type="radio" value="forex" id="forex" name="materiel" className="peer hidden" />
+                        <label htmlFor="forex" className='w-14 flex items-center justify-center cursor-pointer border rounded-lg text-slate-400 peer-checked:text-indigo-500 text-nowrap peer-checked:border-indigo-500 p-2'> Forex</label>
+                    </div>
+                </div>
+
+                <p>sélectionné font</p>
                 <select
                     className="text-black px-3 py-2 rounded-md border border-gray-300 w-full"
                     value={font}
@@ -189,7 +225,7 @@ export default function NeonSign() {
                     ))}
                 </select>
                 <p>sélection range</p>
-                <input type="range" value={range} onChange={(e) => setRange(Number(e.target.value) || 1)} className="mb-5" />
+                <input type="range" value={range} min={1} max={10} onChange={(e) => setRange(Number(e.target.value))} className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer mb-5 dark:bg-gray-700" />
                 {/* Glow Intensity Slider */}
 
                 <button className="bg-primer p-2 w-full rounded-lg hover:bg-second">Commander</button>
