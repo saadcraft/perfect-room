7e2ca81d6b02091761737ff671889fe88cfcf102
saadcraft <39559368+saadcraft@users.noreply.github.com>
2025-07-05 02:27:56 +0100

    update2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Promo from "@/components/promo-section/promo";
 import Faq from "@/components/faq-section/faq";
 
 export default function Home() {
+  console.log(process.env.SERVER_DOMAIN)
   return (
     <div>
       <HeroSection />
